32dd2781c3b451f1a9ec6ec98e81ad197d2b5a61
Mathis S.M <mathis.sema@gmail.com>
2022-06-24 00:47:04 +0200

    Add fontawesome icons

--- src/main.js
+++ src/main.js
@@ -2,5 +2,33 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faDiscord,
+  faTwitch,
+  faYoutube,
+  faTwitter,
+  faInstagram,
+  faFacebookF,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faSortDown, faBars } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(i18n).use(router).mount("#app");
+library.add(
+  faSortDown,
+  faBars,
+  faDiscord,
+  faTwitch,
+  faYoutube,
+  faTwitter,
+  faInstagram,
+  faFacebookF,
+  faLinkedin
+);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(i18n)
+  .use(router)
+  .mount("#app");
