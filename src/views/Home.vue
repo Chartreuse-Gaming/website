459a25d42880b8823f6576dfe46f32925aa8543a
Mathis S.M <mathis.sema@gmail.com>
2022-06-24 17:00:42 +0200

    Add association section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -82,6 +82,32 @@
       </section>
       <router-link to="/" class="btn">Charger plus</router-link>
     </article>
+
+    <article id="association">
+      <img
+        src="@/assets/img/Tshirt.jpg"
+        :alt="$t('association.t-shirt') + '.'"
+      />
+      <i></i>
+      <section class="content">
+        <h2>{{ $t("association.self", 2) }}</h2>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <img
+            src="@/assets/img/Members.jpg"
+            :alt="$t('association.members') + '.'"
+          />
+        </div>
+      </section>
+    </article>
   </main>
 </template>
 
@@ -92,12 +118,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "node_modules/compass-mixins/lib/compass/css3";
+
 main {
   margin-top: 90px;
 }
 
 #old-tournaments {
   h2 {
+    font-size: 1.6em;
     margin-bottom: 16px;
     font-family: var(--title-font);
     text-transform: uppercase;
@@ -167,6 +196,57 @@ main {
       &:hover span i {
         transform: rotate(-5deg);
       }
+    }
+  }
+}
+
+#association {
+  position: relative;
+
+  & > img {
+    filter: opacity(0.25);
+    z-index: -2;
+    position: absolute;
+    width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+    object-position: center bottom;
+  }
+
+  & > i {
+    @include box-shadow(
+      inset 0 100px 50px -30px rgb(38, 38, 38),
+      inset 0 -100px 50px -30px rgb(38, 38, 38)
+    );
+    z-index: -1;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  h2 {
+    font-size: 1.6em;
+    position: absolute;
+    font-family: var(--title-font);
+    text-transform: uppercase;
+  }
+
+  section div {
+    display: flex;
+    align-items: center;
+    gap: 40px;
+
+    p {
+      margin-top: 50px;
+      font-size: 1.2em;
+      text-align: justify;
+    }
+
+    img {
+      width: 44%;
+      aspect-ratio: 16/9;
+      border-radius: 8px;
+      box-shadow: 0 10px 20px rgb(0 0 0 / 19%), 0 6px 6px rgb(0 0 0 / 23%);
     }
   }
 }
