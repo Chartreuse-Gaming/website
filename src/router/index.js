c874a5e0bf0687fcc361271ca1dc62b94f0eeed4
Mathis S.M <mathis.sema@gmail.com>
2022-06-29 15:47:09 +0200

    Add scroll to anchor

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,22 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  scrollBehavior(to, from, SavedPosition) {
+    if (to.hash) {
+      let id = window.location.href.split("#")[1];
+      if (id.length) {
+        let yOffset = -90;
+        let element = document.getElementById(id);
+        let y =
+          element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        window.scrollTo({ top: y, behavior: "smooth" });
+      }
+    } else if (SavedPosition) {
+      return SavedPosition;
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  },
   routes,
 });
 
