3618b37d177b26fecf082dd883406e316e9afe07
Minarox <contact@minarox.fr>
2022-07-06 14:48:00 +0200

    Lint project

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/website/'
-      : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/website/" : "/",
 
   pluginOptions: {
     i18n: {
