cc392fa5e9ec07cac39508d3997460217473e1c5
Minarox <contact@minarox.fr>
2022-07-18 17:22:19 +0200

    Reduce scroll before animation

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -126,7 +126,7 @@ export default {
   mounted() {
     const header = document.getElementsByTagName("header")[0];
     window.onscroll = function () {
-      if (document.documentElement.scrollTop > 100) {
+      if (document.documentElement.scrollTop > 70) {
         header.classList.add("scroll");
       } else {
         header.classList.remove("scroll");
