244438bfc51cc1b711f3caee2f2e2fb4ece0f82e
Minarox <contact@minarox.fr>
2023-10-14 01:50:14 +0200

    build(global): URL et nom de l'association

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://alpes-esport.fr',
     integrations: [mdx(), sitemap()],
     output: 'server',
     adapter: cloudflare(),
