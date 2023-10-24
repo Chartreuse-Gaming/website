a4bd33e9360e61f3e5c710900d5861bc9d47c8ee
Minarox <contact@minarox.fr>
2023-10-25 01:04:07 +0200

    build(global): Recreate project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-
-import cloudflare from '@astrojs/cloudflare'
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://alpes-esport.fr',
-    integrations: [mdx(), sitemap()],
-    output: 'server',
-    adapter: cloudflare(),
-})
+export default defineConfig({});
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alpes Esport'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = "Alpes Esport";
+export const SITE_DESCRIPTION = "Organisateur d'événements Esports en Isère depuis 2016.";
+export const SITE_URL = "https://alpes-esport.fr";
