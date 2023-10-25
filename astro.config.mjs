bc42abc84b4b5a1ab9117bc7c5f43468230500ab
Minarox <contact@minarox.fr>
2023-10-26 01:21:21 +0200

    style(global): Import SCSS variables globally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alpes-esport.fr",
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Import variables globally
+          additionalData: `@import "@/styles/variables.scss";`
+        }
+      }
+    }
+  }
 });
