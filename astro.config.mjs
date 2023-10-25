d27fbce9e0d08198eef137ea075ccbb19ac9cd50
Minarox <contact@minarox.fr>
2023-10-25 13:28:47 +0200

    feat(global): Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
+import { SITE_URL } from "./src/consts.js";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE_URL,
+  integrations: [sitemap()]
+});
