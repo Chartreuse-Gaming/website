ac96f28ea8ef7b9bb09448b4d86760347a212dc1
Minarox <contact@minarox.fr>
2023-10-26 01:06:51 +0200

    refactor(global): Remove comments

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE: string = "Alpes Esport";
 export const SITE_DESCRIPTION: string = "Organisateur d'événements Esports en Isère depuis 2016.";
 export const SITE_URL: string = "https://alpes-esport.fr";
