b0c87232a7ec9887f709a0eb1742a78cc25fb127
Minarox <contact@minarox.fr>
2023-10-28 12:17:42 +0200

    feat(carousel): Add banner

--- src/components/home/carousel.vue
+++ src/components/home/carousel.vue
@@ -1,8 +1,8 @@
 <template>
     <article id="carousel">
         <Splide
-            :options="options"
             :aria-label="$t('tournaments.self')"
+            :options="options"
             @splide:autoplay:play="updateDOM"
             @splide:autoplay:pause="updateDOM"
             @splide:active="updateDOM"
@@ -10,122 +10,116 @@
             <SplideSlide>
                 <article>
                     <section>
-                        <p>{{ $t("tournaments.CEC.6.date") }}</p>
                         <p class="CEC">
-                            Chartreuse<br />
-                            Esport<br />
-                            Contest <span>6</span>
+                            Nous<br />
+                            devenons<br />
+                            Alpes Esport
                         </p>
-                        <router-link :to="{ name: 'CEC6' }" class="btn">
+                        <router-link class="btn" to="/#association">
                             {{ $t("read-more") }}
                             <font-awesome-icon icon="fa-solid fa-right-long" />
                         </router-link>
                     </section>
-                    <img
-                        src="@/assets/img/carousel/CEC/CEC_logo.webp"
-                        :alt="$t('tournaments.CEC.self') + ' logo'"
-                        loading="lazy"
-                    />
                 </article>
                 <img
-                    src="@/assets/img/carousel/CEC/CEC6.webp"
                     :alt="$t('tournaments.CEC.6.self')"
                     loading="lazy"
+                    src="@/assets/img/carousel/Nouveau_nom.webp"
                 />
             </SplideSlide>
             <!--<SplideSlide>
-        <article>
-          <section>
-            <p>{{ $t("tournaments.CEC.5.date") }}</p>
-            <p class="CEC">
-              Chartreuse<br />
-              Esport<br />
-              Contest <span>5</span>
-            </p>
-            <router-link :to="{ name: 'CEC5' }" class="btn">
-              {{ $t("read-more") }}
-              <font-awesome-icon icon="fa-solid fa-right-long" />
-            </router-link>
-          </section>
-          <img
-            src="@/assets/img/carousel/CEC/CEC_logo.webp"
-            :alt="$t('tournaments.CEC.self') + ' logo'"
-          />
-        </article>
-        <img
-          src="@/assets/img/carousel/CEC/CEC5.webp"
-          :alt="$t('tournaments.CEC.5.self')"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <article>
-          <section>
-            <p>{{ $t("tournaments.CEC.4.date") }}</p>
-            <p id="CEC4" class="CEC">
-              Chartreuse<br />
-              Esport<br />
-              Contest <span>4</span>
-              <span>Online</span>
-            </p>
-            <router-link :to="{ name: 'CEC4' }" class="btn">
-              {{ $t("read-more") }}
-              <font-awesome-icon icon="fa-solid fa-right-long" />
-            </router-link>
-          </section>
-          <img
-            src="@/assets/img/carousel/CEC/CEC_logo.webp"
-            :alt="$t('tournaments.CEC.self') + ' logo'"
-          />
-        </article>
-        <img
-          src="@/assets/img/carousel/CEC/CEC4.webp"
-          :alt="$t('tournaments.CEC.4.self')"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <article>
-          <section>
-            <p>{{ $t("tournaments.CEC.3.date") }}</p>
-            <p class="CEC">
-              Chartreuse<br />
-              Esport<br />
-              Contest <span>3</span>
-            </p>
-            <router-link :to="{ name: 'CEC3' }" class="btn">
-              {{ $t("read-more") }}
-              <font-awesome-icon icon="fa-solid fa-right-long" />
-            </router-link>
-          </section>
-          <img
-            src="@/assets/img/carousel/CEC/CEC_logo.webp"
-            :alt="$t('tournaments.CEC.self') + ' logo'"
-          />
-        </article>
-        <img
-          src="@/assets/img/carousel/CEC/CEC3.webp"
-          :alt="$t('tournaments.CEC.3.self')"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <article>
-          <section>
-            <p>{{ $t("tournaments.CGW_2019.date") }}</p>
-            <p>
-              Chartreuse<br />
-              Gaming<br />
-              Winter 2019
-            </p>
-            <router-link :to="{ name: 'CGW_2019' }" class="btn">
-              {{ $t("read-more") }}
-              <font-awesome-icon icon="fa-solid fa-right-long" />
-            </router-link>
-          </section>
-        </article>
-        <img
-          src="@/assets/img/carousel/CGW_2019.webp"
-          :alt="$t('tournaments.CGW_2019.self')"
-        />
-      </SplideSlide>-->
+<article>
+<section>
+<p>{{ $t("tournaments.CEC.5.date") }}</p>
+<p class="CEC">
+Chartreuse<br />
+Esport<br />
+Contest <span>5</span>
+</p>
+<router-link :to="{ name: 'CEC5' }" class="btn">
+{{ $t("read-more") }}
+<font-awesome-icon icon="fa-solid fa-right-long" />
+</router-link>
+</section>
+<img
+src="@/assets/img/carousel/CEC/CEC_logo.webp"
+:alt="$t('tournaments.CEC.self') + ' logo'"
+/>
+</article>
+<img
+src="@/assets/img/carousel/CEC/CEC5.webp"
+:alt="$t('tournaments.CEC.5.self')"
+/>
+</SplideSlide>
+<SplideSlide>
+<article>
+<section>
+<p>{{ $t("tournaments.CEC.4.date") }}</p>
+<p id="CEC4" class="CEC">
+Chartreuse<br />
+Esport<br />
+Contest <span>4</span>
+<span>Online</span>
+</p>
+<router-link :to="{ name: 'CEC4' }" class="btn">
+{{ $t("read-more") }}
+<font-awesome-icon icon="fa-solid fa-right-long" />
+</router-link>
+</section>
+<img
+src="@/assets/img/carousel/CEC/CEC_logo.webp"
+:alt="$t('tournaments.CEC.self') + ' logo'"
+/>
+</article>
+<img
+src="@/assets/img/carousel/CEC/CEC4.webp"
+:alt="$t('tournaments.CEC.4.self')"
+/>
+</SplideSlide>
+<SplideSlide>
+<article>
+<section>
+<p>{{ $t("tournaments.CEC.3.date") }}</p>
+<p class="CEC">
+Chartreuse<br />
+Esport<br />
+Contest <span>3</span>
+</p>
+<router-link :to="{ name: 'CEC3' }" class="btn">
+{{ $t("read-more") }}
+<font-awesome-icon icon="fa-solid fa-right-long" />
+</router-link>
+</section>
+<img
+src="@/assets/img/carousel/CEC/CEC_logo.webp"
+:alt="$t('tournaments.CEC.self') + ' logo'"
+/>
+</article>
+<img
+src="@/assets/img/carousel/CEC/CEC3.webp"
+:alt="$t('tournaments.CEC.3.self')"
+/>
+</SplideSlide>
+<SplideSlide>
+<article>
+<section>
+<p>{{ $t("tournaments.CGW_2019.date") }}</p>
+<p>
+Chartreuse<br />
+Gaming<br />
+Winter 2019
+</p>
+<router-link :to="{ name: 'CGW_2019' }" class="btn">
+{{ $t("read-more") }}
+<font-awesome-icon icon="fa-solid fa-right-long" />
+</router-link>
+</section>
+</article>
+<img
+src="@/assets/img/carousel/CGW_2019.webp"
+:alt="$t('tournaments.CGW_2019.self')"
+/>
+</SplideSlide>-->
         </Splide>
     </article>
 </template>
@@ -167,58 +161,58 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import "node_modules/compass-mixins/lib/compass/css3";
 
 article {
     .splide__slide {
-        min-height: 450px;
         max-height: 700px;
+        min-height: 450px;
 
         &::after {
             @include box-shadow(
                 inset 0 175px 180px 0 var(--bg-color),
                 inset 0 -175px 90px -30px var(--bg-color)
             );
-            height: calc(100% + 2px);
-            content: "";
-            position: absolute;
-            top: -1px;
-            right: 0;
             bottom: 0;
+            content: "";
+            height: calc(100% + 2px);
             left: 0;
+            position: absolute;
+            right: 0;
+            top: -1px;
         }
     }
 
     article {
-        position: relative;
-        margin-inline: auto;
-        width: min(100% - 2rem, 1350px);
         height: 100%;
+        margin-inline: auto;
+        position: relative;
+        width: min(100% - 2rem, 1350px);
 
         section {
-            z-index: 2;
-            position: absolute;
             bottom: 22%;
-            left: 0;
             display: flex;
             flex-flow: column nowrap;
             gap: 4px;
+            left: 0;
+            position: absolute;
+            z-index: 2;
 
             &::before {
-                content: "";
-                z-index: -1;
-                width: calc(100% + 300px);
-                height: calc(100% + 300px);
-                top: -150px;
-                left: -150px;
-                position: absolute;
                 background: rgb(38, 38, 38);
                 background: radial-gradient(
                     circle,
                     rgba(38, 38, 38, 1) 0%,
                     rgba(252, 70, 107, 0) 60%
                 );
+                content: "";
+                height: calc(100% + 300px);
+                left: -150px;
+                position: absolute;
+                top: -150px;
+                width: calc(100% + 300px);
+                z-index: -1;
             }
 
             & > p:first-of-type {
@@ -228,18 +222,18 @@ article {
             }
 
             & > p:last-of-type {
-                position: relative;
+                font-family: var(--title-font);
                 font-size: 3.6em;
+                font-weight: bold;
+                position: relative;
                 text-shadow: 0 0 16px rgba(0, 0, 0, 0.8);
                 text-transform: uppercase;
-                font-family: var(--title-font);
-                font-weight: bold;
             }
 
             a {
-                width: max-content;
                 margin: 20px 0 0;
                 padding: 8px 30px;
+                width: max-content;
 
                 svg {
                     margin-left: 10px;
@@ -250,9 +244,9 @@ article {
                 text-align: left !important;
 
                 span {
-                    position: absolute;
                     font-size: 2.8em;
                     line-height: 0;
+                    position: absolute;
                     right: 0;
                     transform: rotate(8deg);
                 }
@@ -265,21 +259,21 @@ article {
 
                 span:last-of-type {
                     font-size: 0.6em;
-                    top: 0;
                     right: -50px;
+                    top: 0;
 
                     &:before {
-                        z-index: -1;
-                        content: "";
-                        position: absolute;
-                        border-radius: 8px;
                         background-color: var(--bg-color);
                         border: 2px solid var(--special-color);
-                        height: 46px;
-                        top: -25px;
-                        left: -11px;
-                        width: 160px;
+                        border-radius: 8px;
                         box-shadow: rgba(0, 0, 0, 0.4) 0 8px 24px 0;
+                        content: "";
+                        height: 46px;
+                        left: -11px;
+                        position: absolute;
+                        top: -25px;
+                        width: 160px;
+                        z-index: -1;
 
                         @media only screen and (max-width: 1300px) {
                             height: 40px;
@@ -304,22 +298,22 @@ article {
                 }
 
                 & > p:last-of-type {
-                    position: relative;
+                    font-family: var(--title-font);
                     font-size: 3em;
+                    font-weight: bold;
+                    position: relative;
                     text-shadow: 0 0 16px rgba(0, 0, 0, 0.8);
                     text-transform: uppercase;
-                    font-family: var(--title-font);
-                    font-weight: bold;
                 }
             }
 
             @media only screen and (max-width: 450px) {
-                width: 100%;
-                height: calc(100% - 170px);
                 align-items: center;
+                height: calc(100% - 170px);
                 justify-content: center;
-                text-align: center;
                 padding: 0 16px;
+                text-align: center;
+                width: 100%;
 
                 & > p:first-of-type {
                     color: var(--subtext-color);
@@ -328,21 +322,21 @@ article {
                 }
 
                 & > p:last-of-type {
-                    position: relative;
+                    font-family: var(--title-font);
                     font-size: 2.4em;
+                    font-weight: bold;
+                    position: relative;
                     text-shadow: 0 0 16px rgba(0, 0, 0, 0.8);
                     text-transform: uppercase;
-                    font-family: var(--title-font);
-                    font-weight: bold;
                 }
             }
         }
 
         img {
+            bottom: 0;
             height: 80%;
             position: absolute;
             right: -16px;
-            bottom: 0;
             transform: rotate(12deg);
         }
     }
